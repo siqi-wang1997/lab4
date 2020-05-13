948eae7fc8f0540eac069d6140cf732aab859d51
wsq <sq637923@dal.ca>
2020-05-13 12:02:24 -0300

    newfile

--- task2.py
+++ task2.py
@@ -1,1 +1,16 @@
-print(22)
+from string import punctuation, whitespace
+
+book = 'origin.txt'
+
+word = open(book)
+
+def onlyword(word):
+    total = ''
+    for char in word:
+        if ((char in punctuation) or (char in whitespace)):
+            pass
+        else:
+            total += char.lower()
+    return total
+        
+print ("{} has {} 'words'".format(book, len([onlyword(word) for word in words])))
